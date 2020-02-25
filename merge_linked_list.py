b0df6aaa02ed3bf4d9176cf8ce6dd2e5fb29563e
Stanley Jacob <stanleyjacob@users.noreply.github.com>
2020-02-24 22:21:41 -0800

    Add files via upload

---
+++ merge_linked_list.py
@@ -0,0 +1,35 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def mergeLinkedLists(headOne, headTwo):
+    # Write your code here.
+    merged_list = None
+	
+	if headOne.value < headTwo.value:
+		merged_list = headOne
+		headOne = headOne.next
+	else:
+		merged_list = headTwo
+		headTwo = headTwo.next
+	
+	curr_node = merged_list
+	while headOne != None and \
+		headTwo != None:
+		if headOne.value < headTwo.value:
+			curr_node.next = headOne
+			headOne = headOne.next
+		else:
+			curr_node.next = headTwo
+			headTwo = headTwo.next
+		curr_node = curr_node.next
+	
+	if headOne != None:
+		curr_node.next = headOne
+		
+	if headTwo != None:
+		curr_node.next = headTwo
+		
+	return merged_list
