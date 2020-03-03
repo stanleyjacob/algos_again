3edf73269a8aa5bf6df13b408044199589747b82
Stanley Jacob <stanleyjacob@users.noreply.github.com>
2020-03-02 19:02:48 -0800

    Update merge_linked_list.py

--- merge_linked_list.py
+++ merge_linked_list.py
@@ -1,19 +1,19 @@
 
 class LinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+	def __init__(self, value):
+		self.value = value
+		self.next = None
 
 def mergeLinkedLists(headOne, headTwo):
-    merged_list = None
-	
+	merged_list = None
+
 	if headOne.value < headTwo.value:
 		merged_list = headOne
 		headOne = headOne.next
 	else:
 		merged_list = headTwo
 		headTwo = headTwo.next
-	
+
 	curr_node = merged_list
 	while headOne != None and \
 		headTwo != None:
@@ -24,11 +24,11 @@ def mergeLinkedLists(headOne, headTwo):
 			curr_node.next = headTwo
 			headTwo = headTwo.next
 		curr_node = curr_node.next
-	
+
 	if headOne != None:
 		curr_node.next = headOne
-		
+
 	if headTwo != None:
 		curr_node.next = headTwo
-		
+
 	return merged_list
