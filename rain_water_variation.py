fcd0e22507527fd664edcb76e13064de0fc1096c
Stanley Jacob <stanleyjacob@users.noreply.github.com>
2020-09-24 00:00:51 -0500

    Update rain_water_variation.py

--- rain_water_variation.py
+++ rain_water_variation.py
@@ -1,11 +1,11 @@
 
 def maxRight(arr):
-  right_counts = []
-  for i in range(len(arr)):
-    curr_count = 0
-    for j in range(i + 1, len(arr)):
-      if arr[j] < arr[i]:
-        curr_count += 1
-      right_counts.append(curr_count)
-  
-  return right_counts
+    right_counts = []
+    for i in range(len(arr)):
+        curr_count = 0
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[i]:
+                curr_count += 1
+            right_counts.append(curr_count)
+
+    return right_counts
