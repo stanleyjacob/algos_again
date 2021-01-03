24777ace3c922a027a8b25720cdd0f934f56f55d
Stanley Jacob <stanleyjacob@users.noreply.github.com>
2021-01-02 21:57:07 -0800

    Create stock_portfolio.py

---
+++ stock_portfolio.py
@@ -0,0 +1,10 @@
+
+initial_capital = 8000
+monthly_income = 5000
+time_horizon_days = 8
+sectors = ['consumer discretionary']
+tickers = ['AAPL']
+
+def getAveragePrice(start_date, time_length, ticker):
+	pass
+
